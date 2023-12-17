097f89cf56bdc15b7de6befe00f98b71356a3a69
Nicolas Araujo <araujo.n@northeastern.edu>
2023-12-17 02:04:05 -0300

    reformat structure

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,11 +3,38 @@ use std::fs::read_to_string;
 fn main() {
     let path = "src/input.txt";
     let input = read_to_string(path).unwrap();
-    let output = solve(input.as_str());
-    println!("Result: {}", output)
+    let output = solve_p1(input.as_str());
+    println!("Problem One Result: {}", output);
+    let output = solve_p2(input.as_str());
+    println!("Problem Two Result: {}", output);
 }
 
-fn solve(input: &str) -> usize {
+fn solve_p1(input: &str) -> usize {
+    let mut sum: usize = 0;
+    for line in input.lines() {
+        sum += get_calibration_value(line);
+    }
+
+    return sum;
+}
+
+fn get_calibration_value(line: &str) -> usize {
+    let mut first_num: char = '\0';
+    let mut last_num: char = '\0';
+    for char in line.chars() {
+        if char.is_ascii_digit() {
+            if first_num == '\0' {
+                first_num = char;
+            }
+            last_num = char;
+        }
+    }
+
+    let result = format!("{}{}", first_num, last_num);
+    return result.parse().unwrap();
+}
+
+fn solve_p2(input: &str) -> usize {
     let mut sum: usize = 0;
     for line in input.lines() {
         sum += get_advanced_calibrated_value(line);
@@ -109,11 +136,22 @@ fn parse_spelled_digit(segment: &str) -> char {
 }
 
 #[cfg(test)]
-mod p2 {
+mod tests {
     use super::*;
 
     #[test]
-    fn sample() {
+    fn p1_sample() {
+        let example_input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let result = solve_p1(example_input);
+        let expected = 142;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn p2_sample() {
         let example_input = "two1nine
 eightwothree
 abcone2threexyz
@@ -121,7 +159,7 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen";
-        let result = solve(example_input);
+        let result = solve_p2(example_input);
         let expected = 281;
         assert_eq!(result, expected);
     }
--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,13 +1,15 @@
-use std::fs::read_to_string;
+use std::{cmp::max, fs::read_to_string};
 
 fn main() {
     let path = "src/input.txt";
     let input = read_to_string(path).unwrap();
-    let output = solve(input.as_str());
-    println!("Result: {}", output);
+    let output = solve_p1(input.as_str());
+    println!("Problem One Result: {}", output);
+    let output = solve_p2(input.as_str());
+    println!("Problem Two Result: {}", output);
 }
 
-fn solve(input: &str) -> i64 {
+fn solve_p1(input: &str) -> i64 {
     let mut response = 0;
     for line in input.lines() {
         let mut parts = line.split(':');
@@ -63,19 +65,75 @@ fn get_game_id(line: &str) -> i64 {
     return game_id.unwrap().parse().unwrap();
 }
 
+fn solve_p2(input: &str) -> i64 {
+    let mut response = 0;
+    for line in input.lines() {
+        let mut parts = line.split(':');
+        let game_input = parts.nth(1).unwrap();
+        response += power_of_cubes(game_input);
+    }
+    return response;
+}
+
+fn power_of_cubes(game_input: &str) -> i64 {
+    let handfuls = game_input.split(';');
+
+    let mut max_red = 0;
+    let mut max_green = 0;
+    let mut max_blue = 0;
+    for handful in handfuls {
+        let (red, green, blue) = red_green_blue_count(handful);
+        max_red = max(max_red, red);
+        max_green = max(max_green, green);
+        max_blue = max(max_blue, blue);
+    }
+    return max_red * max_green * max_blue;
+}
+
+fn red_green_blue_count(handful_of_cubes: &str) -> (i64, i64, i64) {
+    let mut red_count = 0;
+    let mut green_count = 0;
+    let mut blue_count = 0;
+    let amount_color_pairs = handful_of_cubes.split(',');
+    for amount_color_pair in amount_color_pairs {
+        let mut parts = amount_color_pair.split_whitespace();
+        let amount: i64 = parts.nth(0).unwrap().parse().unwrap();
+        let color = parts.nth(0).unwrap();
+        match color {
+            "red" => red_count += amount,
+            "green" => green_count += amount,
+            "blue" => blue_count += amount,
+            &_ => panic!("unknown color {}", color),
+        }
+    }
+    return (red_count, green_count, blue_count);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn sample() {
+    fn p1_sample() {
         let example_input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        let result = solve(example_input);
+        let result = solve_p1(example_input);
         let expected = 8;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn p2_sample() {
+        let example_input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let result = solve_p2(example_input);
+        let expected = 2286;
         assert_eq!(result, expected);
     }
 }
