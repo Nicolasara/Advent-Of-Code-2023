bd8255353e0e8bbd2be7ca73deb07aa11ba12e9e
Nicolas Araujo <araujo.n@northeastern.edu>
2023-12-20 12:20:55 -0300

    finish day 4 part two

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,5 +1,5 @@
 use scan_fmt::scan_fmt;
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 struct Card {
     index: i64,
@@ -96,8 +96,39 @@ fn contains_number(list: &[i64], number: &i64) -> bool {
     return false;
 }
 
-fn solve_p2(_input: &str) -> i64 {
-    return 0;
+fn solve_p2(input: &str) -> i64 {
+    let cards: Vec<Card> = input.lines().map(|line| parse_card(line)).collect();
+    let mut card_counts = HashMap::new();
+    for card in &cards {
+        card_counts.insert(card.index, 1);
+    }
+
+    for card in &cards {
+        let current_card_count = *card_counts.get(&card.index).unwrap();
+        let cards_to_copy = get_cards_to_copy(&card);
+        for card_to_copy in cards_to_copy {
+            card_counts.insert(
+                card_to_copy,
+                card_counts.get(&card_to_copy).unwrap_or(&0) + current_card_count,
+            );
+        }
+    }
+    return card_counts.values().sum();
+}
+
+fn get_cards_to_copy(card: &Card) -> Vec<i64> {
+    let mut count = 0;
+    for players_number in &card.players_numbers {
+        if contains_number(&card.winning_numbers, players_number) {
+            count += 1;
+        }
+    }
+    let mut card_copy_indexes = Vec::new();
+    let next_card_index = card.index + 1;
+    for card_index in next_card_index..next_card_index + count {
+        card_copy_indexes.push(card_index);
+    }
+    return card_copy_indexes;
 }
 
 #[cfg(test)]
@@ -126,7 +157,32 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     #[test]
     fn p2_sample() {
-        let input = "";
-        test_sample(solve_p2, input, -1);
+        let input1 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let input2 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19";
+        let input3 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1";
+        let input4 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83";
+        let input5 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36";
+        let input6 = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        test_sample(solve_p2, input1, 5);
+        test_sample(solve_p2, input2, 10);
+        test_sample(solve_p2, input3, 19);
+        test_sample(solve_p2, input4, 28);
+        test_sample(solve_p2, input5, 29);
+        test_sample(solve_p2, input6, 30);
     }
 }
